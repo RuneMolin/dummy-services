d21dfc29c35165a60f9059a6a6104b9de6d7fad3
poul jacobsen <pjacobs2@csc.com>
2016-05-10 13:29:34 +0000

    initial commit

---
+++ src/main/java/dk/cpr/dar/DummyServicesApplication.java
@@ -0,0 +1,12 @@
+package dk.cpr.dar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyServicesApplication.class, args);
+	}
+}
