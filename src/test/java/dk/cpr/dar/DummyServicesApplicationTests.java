9c376944240e2ef363b0182529efce0d95adf922
poul jacobsen <pjacobs2@csc.com>
2016-05-30 13:37:46 +0000

    deploy: first cut

--- src/test/java/dk/cpr/dar/DummyServicesApplicationTests.java
+++ src/test/java/dk/cpr/dar/DummyServicesApplicationTests.java
@@ -1,17 +1,8 @@
 package dk.cpr.dar;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DummyServicesApplication.class)
-@WebAppConfiguration
 public class DummyServicesApplicationTests {
 
-	@Test
+
 	public void contextLoads() {
 	}
 
