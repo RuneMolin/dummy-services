131dc1f927e56c535054c4ec6739a0c0de2caddb
poul jacobsen <pjacobs2@csc.com>
2016-05-31 11:34:56 +0000

    deploy: fix logdir

---
+++ src/main/java/dk/cpr/dar/package-info.java
@@ -0,0 +1,12 @@
+/**
+*
+* # Dummy service
+*
+* This component simulates EventStatus service ADRaAReplikeringEventStatusHent.
+* By default it will startup on port 9080.
+* for initial and full load are identical; However handling by the Datahandler will be different.
+*
+* Use **get** method for reading/testing the dummyservice (e.g. http://localhost:9080/ADRaAReplikeringEventStatusHent)
+* Use **post** method for setting the response
+**/
+package dk.cpr.dar;
