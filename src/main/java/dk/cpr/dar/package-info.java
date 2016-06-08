20b723b16cd68944b78c4dd62f1f01e48c17cced
pjacobs2 <pjacobs2@csc.com>
2016-06-08 14:00:25 +0200

    updated javadoc

--- src/main/java/dk/cpr/dar/package-info.java
+++ src/main/java/dk/cpr/dar/package-info.java
@@ -8,5 +8,27 @@
 *
 * Use **get** method for reading/testing the dummyservice (e.g. http://localhost:9080/ADRaAReplikeringEventStatusHent)
 * Use **post** method for setting the response
+*
+* ### To set ADRaAReplikeringEventStatusHent:
+* POST a json snippet using POSTMAN plugin in Chrome Browser like this:
+* [
+* {
+* "entitet": "Adresse",
+*  "eventid": 2
+*  },
+*  {
+*  "entitet": "Sogneinddeling",
+*  "eventid": 2
+*  }
+*  ]
+*
+* ### To set HTTPSTATUS ADRaAReplikeringEventStatusHent:
+* POST a json snippet using POSTMAN plugin in Chrome Browser like this (e.g. 400 - Bad request):
+*
+* [
+* {
+* "httpStatus": "400"
+* }
+* ]
 **/
 package dk.cpr.dar;
